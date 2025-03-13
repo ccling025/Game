0577bed6c251f9ffc7a85e660d4167ee4b8b2ef8
ccling025 <as0950926@gmail.com>
2025-03-13 15:05:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import sys
 import pygame
 
-# 初始化
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Hello World:)')
-screen.fill((255, 255, 255))
-clock = pygame.time.Clock()
+class main():
 
-# 事件迴圈監聽事件，進行事件處理
-while True:
-    # 迭代整個事件迴圈，若有符合事件則對應處理
-    for event in pygame.event.get():
-        # 當使用者結束視窗，程式也結束
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def __init__(self):
+        # 初始化
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption('Hello World:)')
+        self.screen.fill((255, 255, 255))
+        self.clock = pygame.time.Clock()
 
-    pygame.display.update()
-    clock.tick(60)
+asdadawjadnwlkndalkjdwn
+    def run(self):
+        # 事件迴圈監聽事件，進行事件處理
+        while True:
+            # 迭代整個事件迴圈，若有符合事件則對應處理
+            for event in pygame.event.get():
+                # 當使用者結束視窗，程式也結束
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+main().run()
